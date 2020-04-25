c253fccdea0cc25daa013b0d6b253552bde1cbea
aixan <admin@aixan.cn>
2020-04-25 21:52:34 +0800

    删除重复文件

--- nginx/docker安装.sh
+++
@@ -1,1 +0,0 @@
-docker run --name nginx -t -p 80:80 -p 443:443 -v /data/nginx/html:/usr/share/nginx/html -v /data/nginx/conf/nginx.conf:/etc/nginx/nginx.conf -v /data/nginx/logs:/var/log/nginx -v /data/nginx/conf.d:/etc/nginx/conf.d -v /data/nginx/ssl:/etc/nginx/ssl --link php-fpm:php-fpm -d nginx:latest 
